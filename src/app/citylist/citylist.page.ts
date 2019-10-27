ddde780a9dcf615a8bd990628e95ec57b57508d0
mitzsu <timusiiit123@gmail.com>
2019-10-27 23:41:11 +0530

    Added citylist page

---
+++ src/app/citylist/citylist.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-citylist',
+  templateUrl: './citylist.page.html',
+  styleUrls: ['./citylist.page.scss'],
+})
+export class CitylistPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
