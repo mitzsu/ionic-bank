a7155cb664d90fc6afe579592f9c0e02a9545d2c
mitzsu <timusiiit123@gmail.com>
2019-10-29 00:17:49 +0530

    Search Bar is now working.

--- src/app/citylist/citylist.page.ts
+++ src/app/citylist/citylist.page.ts
@@ -27,4 +27,20 @@ export class CitylistPage implements OnInit {
       'dismissed': true
     });
   }
+
+  changeList(event){
+    let query = event.target.value.toLowerCase();
+    const items = Array.from(document.querySelector('ion-list').children);
+    if(query===''){
+      for(let i=0; i<items.length; i++){
+        items[i].setAttribute('class',"show-item item ios in-list ion-focusable hydrated");
+      }
+    }
+    else{
+      for(let i=0; i<items.length; i++){
+        let shouldShow = items[i].textContent.toLowerCase().indexOf(query) > -1;
+        items[i].setAttribute('class',((shouldShow)?"show-item":"hide-item")+" item ios in-list ion-focusable hydrated");
+      }
+    }
+  }
 }
